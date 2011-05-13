abe326291018eedf0f2bf02ff4834b9ca3f832e5
tormaroe <torbjorn.maro@gmail.com>
2011-05-13 08:41:39 +0200

    Added lein-clojars dependencies in order to push to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject bf-clj "1.0.0"
   :description "An adaptable Brainf*ck interpreter"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
